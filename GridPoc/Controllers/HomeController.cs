91003dcf6b24623428c13dcc09954e23b100602e
VenkateshSrinivasan <venkatesh-5.srinivasan-5@cognizant.com>
2017-05-24 19:45:45 +0530

    Code changes for export to excel

--- GridPoc/Controllers/HomeController.cs
+++ GridPoc/Controllers/HomeController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using GridPoc.ViewModel;
 using System.Globalization;
+using OfficeOpenXml;
+using OfficeOpenXml.Style;
 namespace GridPoc.Controllers
 {
     public class HomeController : Controller
@@ -14,6 +16,48 @@ namespace GridPoc.Controllers
             
             return View(GetInsuranceDetails());
         }
+        public IActionResult ExportIndex()
+        {
+            using (ExcelPackage package = new ExcelPackage())
+            {
+                Int32 row = 2;
+                Int32 col = 1;
+               ExcelWorksheet dataSheet =  package.Workbook.Worksheets.Add("Data");
+                
+                dataSheet.Cells[1, 1].Value = "Policy No.";
+                
+                dataSheet.Cells[1, 2].Value = "Policy Type";
+                dataSheet.Cells[1, 3].Value = "Procurement Date";
+                dataSheet.Cells[1, 4].Value = "Effective Date";
+                dataSheet.Cells[1, 5].Value = "Expiry Date";
+                dataSheet.Cells[1, 6].Value = "Agency ID";
+                dataSheet.Cells[1, 7].Value = "Insured";
+                var dataSet = GetInsuranceDetails();
+                for(int objIndex=0; objIndex<dataSet.Count;objIndex++)
+                {
+                    dataSheet.Cells[row, 1].Value = dataSet[objIndex].PolicyNumber.ToString();
+                    dataSheet.Cells[row, 2].Value = dataSet[objIndex].PolicyType;
+                    dataSheet.Cells[row, 3].Value = dataSet[objIndex].ProcurementDate.ToString("mm/dd/yyyy");
+                    dataSheet.Cells[row, 4].Value = dataSet[objIndex].EffectiveDate.ToString("mm/dd/yyyy");
+                    dataSheet.Cells[row, 5].Value = dataSet[objIndex].ExpiryDate.ToString("mm/dd/yyyy");
+                    dataSheet.Cells[row, 6].Value = dataSet[objIndex].AgencyID.ToString();
+                    dataSheet.Cells[row, 7].Value = dataSet[objIndex].NamedInsurer;
+                    row++;
+
+                }
+                dataSheet.Column(1).BestFit = true;
+                dataSheet.Column(2).BestFit = true;
+                dataSheet.Column(3).BestFit = true;
+                dataSheet.Column(4).BestFit = true;
+                dataSheet.Column(5).BestFit = true;
+                dataSheet.Column(6).BestFit = true;
+                dataSheet.Column(7).BestFit = true;
+                
+                return File(package.GetAsByteArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet","policies.xlsx");
+
+            }
+        }
+
 
         public IActionResult About()
         {
